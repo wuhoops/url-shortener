85ec315a6109da0a6e82384a7128a48de6bbc0e6
wuhoops <pasinun.w@gmail.com>
2023-12-03 13:15:16 +0700

    [ADD] auto short key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,19 @@
 import { Input, Button, message, Spin } from "antd";
 import { useState } from "react";
 import supabase from "@/utils/supabaseInstance";
+import { nanoid } from "nanoid";
 
 const Home = () => {
   const [fullLink, setFullLink] = useState("");
   const [shortLink, setShortLink] = useState("");
   const [shortenLink, setShortenLink] = useState("");
   const [messageApi, contextHolder] = message.useMessage();
+  const [isFullLinkInputError, setIsFullLinkInputError] = useState(false);
   const [isShortLinkInputError, setIsShortLinkInputError] = useState(false);
   const [isSubmitWaiting, setIsSubmitWaiting] = useState(false);
 
   const handleFullLinkChange = (fullLink: string) => {
+    setIsFullLinkInputError(false);
     setFullLink(fullLink);
   };
   const handleShortLinkChange = (shortLink: string) => {
@@ -26,34 +29,45 @@ const Home = () => {
     });
   };
   const handleShortenSubmit = async () => {
-    setIsSubmitWaiting(true);
-    await supabase
-      .from("link_map")
-      .insert({ full_link: fullLink, shorten_link: shortLink })
-      .select()
-      .then((res: any) => {
-        setIsSubmitWaiting(false);
-        if (res.error) {
-          if (res.error.code == "23505") {
-            setIsShortLinkInputError(true);
-            messageApi.open({
-              type: "error",
-              content: "This key is already in use.",
-            });
-          }
-        } else {
-          messageApi.open({
-            type: "success",
-            content: "Shorten link successfully!!",
-          });
-          setShortenLink(res.data[0].shorten_link);
-          setFullLink("");
-          setShortLink("");
-        }
+    if (!fullLink.trim()) {
+      setIsFullLinkInputError(true);
+      messageApi.open({
+        type: "error",
+        content: "Please fill the full link.",
       });
+    } else {
+      setIsSubmitWaiting(true);
+      await supabase
+        .from("link_map")
+        .insert({
+          full_link: fullLink,
+          shorten_link: shortLink.trim() ? shortLink.trim() : nanoid(4),
+        })
+        .select()
+        .then((res: any) => {
+          setIsSubmitWaiting(false);
+          if (res.error) {
+            if (res.error.code == "23505") {
+              setIsShortLinkInputError(true);
+              messageApi.open({
+                type: "error",
+                content: "This short link is already in use.",
+              });
+            }
+          } else {
+            messageApi.open({
+              type: "success",
+              content: "Shorten link successfully!!",
+            });
+            setShortenLink(res.data[0].shorten_link);
+            setFullLink("");
+            setShortLink("");
+          }
+        });
+    }
   };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-4">
       {contextHolder}
       <p className="text-2xl font-semibold">URL shortener</p>
       <Input
@@ -65,6 +79,11 @@ const Home = () => {
         autoFocus
         placeholder="Enter the full link here"
         className="max-w-xl"
+        style={{
+          borderColor: isFullLinkInputError ? "red" : "",
+          borderWidth: isFullLinkInputError ? "2px" : "",
+          transition: "none",
+        }}
       />
       <Input
         value={shortLink}
@@ -73,7 +92,7 @@ const Home = () => {
         }}
         size="large"
         autoFocus
-        placeholder="Enter your short link here"
+        placeholder="Enter your short link here (Leave it empty to generate a random key)"
         className="max-w-xl"
         style={{
           borderColor: isShortLinkInputError ? "red" : "",
@@ -81,11 +100,6 @@ const Home = () => {
           transition: "none",
         }}
       />
-      {isShortLinkInputError ? (
-        <p className="text-red-500 self">This key is already in use.</p>
-      ) : (
-        <></>
-      )}
       <Button
         type="primary"
         size="large"
@@ -100,7 +114,6 @@ const Home = () => {
         <div className="mt-20 flex flex-col items-center gap-4">
           <p className="text-4xl">{`s.wuhoops.com/${shortenLink}`}</p>
           <Button
-            type="primary"
             size="large"
             className="bg-blue-500"
             onClick={() => {
